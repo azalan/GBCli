7741794e5e4505186a4d76c633c29d42cbee88bd
azalan <zalan.arpad@gmail.com>
2015-10-05 09:57:54 +0200

    Add framework

---
+++ GBCli.framework/GBCli.framework.h
@@ -0,0 +1,23 @@
+//
+//  GBCli.framework.h
+//  GBCli.framework
+//
+//  Created by Arpad Zalan on 02/10/15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GBCli.framework.
+FOUNDATION_EXPORT double GBCli_frameworkVersionNumber;
+
+//! Project version string for GBCli.framework.
+FOUNDATION_EXPORT const unsigned char GBCli_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GBCli_framework/PublicHeader.h>
+
+#import "GBCli.h"
+#import "GBCommandLineParser.h"
+#import "GBOptionsHelper.h"
+#import "GBSettings.h"
+#import "GBPrint.h"
